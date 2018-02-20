6dbac09e6dec5b5b2efd124d611684fd399bb7d1
Konrad Aust <konrad.aust@gmail.com>
2018-02-19 17:40:00 -0700

    Added better config validation

--- app/config.js
+++ app/config.js
@@ -12,11 +12,31 @@ var config = convict({
   logging: bot.configSchema.logging,
   rabbit: bot.configSchema.rabbit,
   neo4j: bot.configSchema.neo4j,
+  tasks: {
+    doc: "Array of scheduled task definitions.",
+    default: [],
+    format: function check(tasks) {
+      tasks.forEach((task) => {
+        if((typeof task.name) !== 'string' || (typeof task.query) !== 'string' || (typeof task.cronTime) !== 'string') 
+          throw new Error('Tasks must have a name, query, and cronTime property.');
+
+        if(task.desc && typeof task.desc !== 'string') 
+          throw new Error('Task description must be a string');
+
+        if(task.timeZone && typeof task.timeZone !== 'string') 
+          throw new Error('Timezone must be a string');
+      
+        if(task.disable !== undefined && typeof task.disable !== 'boolean') 
+          throw new Error('Disable must be a boolean');
+      })
+    }
+  }
 })
 
 // Load from file.
 if (fs.existsSync('./config/config.json')) {
   config.loadFile('./config/config.json');
+  config.validate();
 }
 
 // Export the config.
