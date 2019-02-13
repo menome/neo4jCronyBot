a256ca49644f5d3df2395d202bdc481dc7b39360
Kent Brockman <brockman.ks@gmail.com>
2019-02-12 20:07:34 -0700

    force neo4j enable, throw more specific configuration errors

--- app/app.js
+++ app/app.js
@@ -17,6 +17,14 @@ require("fs").readdirSync(normalizedPath).forEach(function(file) {
   paths = Object.assign(paths,require("./controllers/" + file).swaggerDef);
 });
 
+// cronybot is designed to run against neo4j, so make sure configuration is provided and enabled
+if (!config.neo4j) {
+  throw "config.neo4j not defined - define neo4j connection parameters"
+}
+
+// cronybot always uses neo4j, make sure it is enabled
+config.neo4j.enable = true;
+
 var bot = new Bot({
   config: {
     name: "Neo4j Crony Bot",
@@ -40,3 +48,9 @@ bot.registerPaths(paths,__dirname+"/controllers");
 
 bot.start();
 bot.changeState({state: "idle"})
+
+// consider surfacing this in a health check of some kind instead
+// right now its just failing on first use, which im too impatient to wait for
+if (!bot.neo4j) {
+  throw "neo4j not defined - review neo4j configuration and ensure it is working"
+}
